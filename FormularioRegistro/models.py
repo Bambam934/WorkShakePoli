f120a60b9b2918c3e1e373dc0211e81450129ac6
Jose <joosedaviis934@gmail.com>
2025-02-28 16:37:07 -0500

    Test de Formulario y solución al Error de la fecha del formulario (Todo ya esta integrado con la base de datos)

--- FormularioRegistro/models.py
+++ FormularioRegistro/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
 
-# Create your models here.
+# Create your models here.from django.db import models
+
 class Registro(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    telefono = models.CharField(max_length=15)
+    email = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
-
+    fecha = models.DateField(auto_now=True)
+    
     objects = models.Manager()
--- FormularioRegistro/views.py
+++ FormularioRegistro/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.hashers import make_password
 from .forms import RegistroForm
-
+from datetime import date
 
 def registro(request):
     if request.method == 'POST':
@@ -9,10 +9,12 @@ def registro(request):
         if form.is_valid():
             nuevo_registro = form.save(commit=False)
             nuevo_registro.password = make_password(form.cleaned_data['password'])  # Encripta la contraseña
+            nuevo_registro.fecha = date.today()
             nuevo_registro.save()
-            return redirect('exito')
+            return redirect('exito')  # Asegúrate de que este 'name' esté en urls.py
     else:
         form = RegistroForm()
+    
     return render(request, 'registro.html', {'form': form})
 
 def exito(request):
