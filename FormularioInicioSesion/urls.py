e31144c752df9d0978e724d5568e88a5596bca1f
Javier <janortiz@poligran.edu.co>
2025-03-08 12:58:47 -0500

    Se relacionan los botones con los formularios

--- FormularioInicioSesion/urls.py
+++ FormularioInicioSesion/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import inicio, inicioExitoso
+from FormularioRegistro import views
 
 urlpatterns = [
     path('iniciarSesion/', inicio, name='inicioSesion'),
     path('inicioExitoso/', inicioExitoso, name='exito'),
+    path('registro/', views.registro, name='registro'),
 ]
--- FormularioRegistro/urls.py
+++ FormularioRegistro/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import home, registro, exito  # Importa la vista home
+from FormularioInicioSesion import views
 
 urlpatterns = [
     path('', home, name='home'),  # Ruta raíz
     path('registro/', registro, name='registro'),
     
     path('exito/', exito, name='exito'),
+    path('iniciarSesion/', views.inicio,name='inicio'),
 ]
