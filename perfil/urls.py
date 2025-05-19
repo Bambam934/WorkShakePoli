699c9dad7b8e8e4a29806f6e0a8f1db23f2b23cf
IDarwinOrtizI <sebastiancaballero0811@gmail.com>
2025-05-18 19:07:57 -0500

    Corrige nombre de URL perfil

--- perfil/urls.py
+++ perfil/urls.py
@@ -1,8 +1,9 @@
+# perfil/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-     path("", views.perfilView, name="Perfil"),                 # GET  /perfil/
+     path("", views.perfilView, name="detalle"),                # GET  /perfil/
      path("upload-profile-picture/", views.upload_profile_picture,
           name="upload_profile"),                               # POST /perfil/upload-profile-picture/
      path("update-color/", views.update_neon_color,
