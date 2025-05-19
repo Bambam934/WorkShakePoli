5108cd67e7a8d603d4a9c06ddb3b6586a1506859
IDarwinOrtizI <sebastiancaballero0811@gmail.com>
2025-05-18 19:01:25 -0500

    Corrige nombre de URL para que coincida con 'Perfil

--- perfil/urls.py
+++ perfil/urls.py
@@ -1,13 +1,12 @@
-# perfil/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.perfilView, name="detalle"),                # GET  /perfil/
-    path("upload-profile-picture/", views.upload_profile_picture,
-         name="upload_profile"),                               # POST /perfil/upload-profile-picture/
-    path("update-color/", views.update_neon_color,
-         name="update_neon_color"),                            # POST
-    path("equip-skin/<int:skin_id>/", views.equip_skin,
-         name="equip_skin"),                                   # POST
+     path("", views.perfilView, name="Perfil"),                 # GET  /perfil/
+     path("upload-profile-picture/", views.upload_profile_picture,
+          name="upload_profile"),                               # POST /perfil/upload-profile-picture/
+     path("update-color/", views.update_neon_color,
+          name="update_neon_color"),                            # POST
+     path("equip-skin/<int:skin_id>/", views.equip_skin,
+          name="equip_skin"),                                   # POST
 ]
